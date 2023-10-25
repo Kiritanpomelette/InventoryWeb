9cdacc10fec40876def3c1126083167f5541b25f
shuto-school <97936840+shuchann@users.noreply.github.com>
2023-10-25 10:56:06 +0900

    ハンバーガーメニューのボタン遷移をjsに変更した。その際にindexで使っていた画面遷移一つが邪魔になったのでindex.jsを作成し、書き込んだ

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // webPC画面
     const indexButton = document.getElementById('toIndexButton');
     const productsButton = document.getElementById('toProductsButton');
-    const productsdetailButton = document.getElementById('toProductsDetailButton');
     // webMB画面
     const hmindexButton = document.getElementById('hm-toIndexButton');
     const hmproductsButton = document.getElementById('hm-toProductsButton');
@@ -14,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
     productsButton.addEventListener('click', function() {
         window.location.href = 'Syohin.html';
     });
-
     // webMB画面遷移(ハンバーガーメニュー)
     hmindexButton.addEventListener('click', function (){
         window.location.href = 'index.html';

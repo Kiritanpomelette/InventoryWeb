ae1b69fc0bd3d388a55b184ff04fa1dee0642ad5
Miyayu <yuchan2215@gmail.com>
2023-11-15 01:00:46 +0900

    add: 詳細画面を追加

--- js/detail.js
+++ js/detail.js
@@ -16,18 +16,38 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 // データの取得に成功したら表を生成する
                 document.getElementById("name").textContent = data.name + "詳細画面";
-                document.getElementById("nownumber").textContent = "現在の個数：" + data.nownumber;
-                document.getElementById("daynumber").textContent = "今日：" + data.daynumber;
-                document.getElementById("weeknumber").textContent = "今週：" + data.weeknumber;
-                document.getElementById("monthnumber").textContent = "今月：" + data.monthnumber;
-                document.getElementById("memo").textContent = "メモ：" + data.memo;            })
+                document.getElementById("memo").textContent = "メモ：" + data.memo;
+            })
             .catch(error => {
                 console.error('Fetching error:', error);
             });
+        function readAndWriteCount(url, element,prefix) {
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    element.innerText = prefix + data.total;
+                })
+                .catch(error => {
+                    console.error('Fetching error:', error);
+                });
+        }
+        const nowNumberElement = document.getElementById("nownumber")
+        const dayNumberElement = document.getElementById("daynumber");
+        const weekNumberElement = document.getElementById("weeknumber");
+        const monthNumberElement = document.getElementById("monthnumber");
+        readAndWriteCount('https://n3.miyayu.xyz/InventoryServer-0.0.1-SNAPSHOT-plain/treasurer/allStatistics?productId=' + id, nowNumberElement,"現在:");
+        readAndWriteCount('https://n3.miyayu.xyz/InventoryServer-0.0.1-SNAPSHOT-plain/treasurer/dailyStatistics?productId=' + id, dayNumberElement,"今日:");
+        readAndWriteCount('https://n3.miyayu.xyz/InventoryServer-0.0.1-SNAPSHOT-plain/treasurer/weeklyStatistics?productId=' + id, weekNumberElement,"今週:");
+        readAndWriteCount('https://n3.miyayu.xyz/InventoryServer-0.0.1-SNAPSHOT-plain/treasurer/monthlyStatistics?productId=' + id, monthNumberElement,"今月:");
     }
-// 取得してきたidのデータの取得
+    // 取得してきたidのデータの取得
     const id = getQueryParam("id");
-// データを表示していく
+    // データを表示していく
     getAllData()
 
 });
